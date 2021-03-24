a3651162aa116ef78b6f164327ed20685fc9745f
zhenya <choshina@hotmail.com>
2021-03-24 17:31:45 +0900

    main

--- phi_Train.m
+++ phi_Train.m
@@ -5,64 +5,102 @@ InitBreach;
 k=0.1;
 c=8;
 
-num_train = 9;
+num_train = 5;
+fixedM = true;
+
+cpt = 10;
+Mrange = [80 120];
+Urange = [-20 20];
 
 addpath model/
 addpath spec/
 
-mdl = 'Train';
+mdl = strcat('Train', num2str(num_train));
 Br = BreachSimulinkSystem(mdl);
 
 Br.Sys.tspan = 0:.01: 50;
 
 input_gen.type = 'UniStep';
-input_gen.cp = 10;
+input_gen.cp = cpt;
 Br.SetInputGen(input_gen);
 
-%for nsi = 1:num_train
-%    Br.SetParam(strcat('M_', num2str(nsi)), 80);
-%end
 
-for nsi = 1:num_train
-    Br.SetParamRanges(strcat('M_', num2str(nsi)), [80 120]);
+if fixedM == true
+    for nsi = 1:num_train
+        Br.SetParam(strcat('M_', num2str(nsi)), Mrange(1));
+    end
+else
+    for nsi = 1:num_train
+        Br.SetParamRanges(strcat('M_', num2str(nsi)), Mrange);
+    end
 end
+
 
 for cpi = 0:input_gen.cp-1
     u_sig = strcat('u_u', num2str(cpi));
-    Br.SetParamRanges({u_sig},[-20 20]);
+    Br.SetParamRanges({u_sig}, Urange);
 end
 
-phi = STL_Formula('phi1','alw_[0,50](Z_1[t] > Z_2[t] and Z_2[t] > Z_3[t] and Z_3[t] > Z_4[t] and Z_4[t] > Z_5[t] and Z_5[t] > Z_6[t]) and Z_6[t] > Z_7[t] and Z_7[t] > Z_8[t]');
 
-IterNum = 1;
 
-total_time = 0.0;
-succ_iter = 0;
-succ_trial = 0;
-obj_best = [];
-num_sim = [];
+for i = 1:3
+    
+    stlf = strcat('spec/', num2str(num_train), num2str(i));
+    phi = STL_ReadFile(stlf);
+    %phi = STL_Formula('phi1','alw_[0,50](Z_1[t] > Z_2[t] and Z_2[t] > Z_3[t] and Z_3[t] > Z_4[t] and Z_4[t] > Z_5[t] and Z_5[t] > Z_6[t]) and Z_6[t] > Z_7[t] and Z_7[t] > Z_8[t]');
 
-for n = 0:IterNum-1
+    max_trials = 10;
+
+    succ = [];
+    obj_best = [];
+    num_sim = []; 
+    total_time = [];
+    counts = [];
     
-    falsif_pb = FalsificationProblem(Br, phi);
-    falsif_pb.max_time = 200;
-    %falsif_pb.max_obj_eval = 20;
-    falsif_pb.setup_solver('cmaes');
-    falsif_pb.solve();
-    
-    
-    if falsif_pb.obj_best < 0
-       total_time = total_time + falsif_pb.time_spent;
-       succ_iter = succ_iter + falsif_pb.nb_obj_eval;
-       succ_trial = succ_trial + 1; 
+    c = 0;
+    time = 0;
+    sims = 0;
+    while true
+        c = c + 1;
+        
+        falsif_pb = FalsificationProblem(Br, phi);
+        falsif_pb.max_time = 200;
+        falsif_pb.setup_solver('cmaes');
+        falsif_pb.solve();
+
+        time = time + falsif_pb.time_spent;
+        sims = sims + falsif_pb.nb_obj_eval;
+        if falsif_pb.obj_best < 0
+           succ = [succ; 1];
+           counts = [counts; c];
+           %obj_best = [obj_best; falsif_pb.obj_best];
+           num_sim = [num_sim; sims];
+           total_time = [total_time; time];
+           break;
+        end
+        
+        if c == max_trials
+            succ = [succ; 0];
+            counts = [counts; -1];
+            %obj_best
+            num_sim = [num_sim; sims];
+            total_time = [total_time; time];
+            break;
+        end
     end
-    obj_best = [obj_best; falsif_pb.obj_best]
-    num_sim = [num_sim; falsif_pb.nb_obj_eval]
-    
-    
+    res = table(succ, counts, num_sim, total_time);
+    filename =  strcat('results/', mdl, '_', stlf, '_', fixed(fixedM), '_', to_str(Mrange), '_', to_str(Urange), '_', num2str(cpt), '.csv');
+    writetable(res, filename,'Delimiter',';');
 end
 
-aver_time = total_time/succ_trial
-aver_succ_iter = succ_iter/succ_trial
-succ_trial
+function str = to_str(r)
+    str = strcat(num2str(r(1)), 'w', num2str(r(2)));
+end
 
+function str = fixed(f)
+    if f
+        str = 'fixed';
+    else
+        str = 'unfixed';
+    end
+end
